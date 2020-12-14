04e6ecf66096d7ab51db9ba6bd319cc870c5150b
Vyacheslav Yastrebov <reconrus@yandex.ru>
2020-12-14 16:18:17 +0500

    Added single OWASP ASVS file

--- get_dataset.sh
+++ get_dataset.sh
@@ -4,26 +4,41 @@ mkdir RawDatasets
 cd RawDatasets
 
 # SecReq Dataset
-wget -O SecReq.zip https://www.dropbox.com/sh/mcvx5ium0zx7bly/AABfJaFt0nWvjiNJs1RUYf_Pa?dl=1
-unzip SecReq.zip -d SecReq
-rm SecReq.zip
+if [ ! -d SecReq ]; then
+    wget -O SecReq.zip https://www.dropbox.com/sh/mcvx5ium0zx7bly/AABfJaFt0nWvjiNJs1RUYf_Pa?dl=1
+    unzip SecReq.zip -d SecReq
+    rm SecReq.zip
+fi
 
 # # PROMISE Dataset
-wget -O Promise.tar https://zenodo.org/record/268542/files/nfr.tar?download=1
-tar -xvf Promise.tar
-rm Promise.tar
-sed -i '45s/.*/@ATTRIBUTE class {F,A,L,LF,MN,O,PE,SC,SE,US,FT,PO}/' ./nfr/nfr.arff
+if [ ! -d nfr ]; then
+    wget -O Promise.tar https://zenodo.org/record/268542/files/nfr.tar?download=1
+    tar -xvf Promise.tar
+    rm Promise.tar
+    sed -i '45s/.*/@ATTRIBUTE class {F,A,L,LF,MN,O,PE,SC,SE,US,FT,PO}/' ./nfr/nfr.arff
+fi
 
 # Concordia Dataset
-wget -O Concordia.tar.gz https://www.semanticsoftware.info/system/files/NFRClassifier.tar.gz
-tar -xzf Concordia.tar.gz
-rm Concordia.tar.gz
+if [ ! -d NFRClassifier ]; then
+    wget -O Concordia.tar.gz https://www.semanticsoftware.info/system/files/NFRClassifier.tar.gz
+    tar -xzf Concordia.tar.gz
+    rm Concordia.tar.gz
+fi
 
 # CCHIT Dataset
-wget -O CCHIT.xls https://www.dropbox.com/s/7pe4xq0ntwbbrlx/CCHIT%20Certified%202011%20Ambulatory%20EHR%20Criteria%2020110517.xls?dl=1
+if [ ! -f CCHIT.xls ]; then
+    wget -O CCHIT.xls https://www.dropbox.com/s/7pe4xq0ntwbbrlx/CCHIT%20Certified%202011%20Ambulatory%20EHR%20Criteria%2020110517.xls?dl=1
+fi
+
+# OWASP Application Security Verification Standard
+if [ ! -f OWASP.csv ]; then
+    wget -O OWASP.csv https://raw.githubusercontent.com/OWASP/ASVS/v4.0.2/4.0/docs_en/OWASP%20Application%20Security%20Verification%20Standard%204.0.2-en.csv
+fi
 
 cd ..
-python3 -m venv env
+[ ! -d env ] && python3 -m venv env
 . ./env/bin/activate
 pip3 install pandas liac-arff xlrd
-python3 prepare_data.py --sec_req ./RawDatasets/SecReq --promise ./RawDatasets/nfr/nfr.arff --concord ./RawDatasets/NFRClassifier/gate/application-resources/Requirements/ --cchit ./RawDatasets/CCHIT.xls
+python3 prepare_data.py --sec_req ./RawDatasets/SecReq --promise ./RawDatasets/nfr/nfr.arff \
+    --concord ./RawDatasets/NFRClassifier/gate/application-resources/Requirements/ \
+    --cchit ./RawDatasets/CCHIT.xls --owasp ./RawDatasets/OWASP.csv
--- prepare_data.py
+++ prepare_data.py
@@ -1,10 +1,14 @@
 import os
+import re
 
 import arff
 import argparse
 import pandas as pd
 
 from xml.etree import ElementTree as ET
+
+SEC_LABEL = "sec"
+NONSEC_LABEL = "nonsec"
 
 parser = argparse.ArgumentParser(
     description="Data unification script for Security Requirements Extraction task"
@@ -34,6 +38,12 @@ parser.add_argument(
     help="Path to CCHIT Excel Sheet",
 )
 parser.add_argument(
+    "--owasp",
+    default="./Datasets/OWASP.csv",
+    type=str,
+    help="Path to OWASP Application Security Verification Standard csv file",
+)
+parser.add_argument(
     "-o",
     default="result.csv",
     type=str,
@@ -59,12 +69,13 @@ def read_seqreq(path, resulting_dataset):
             engine="python",
         )
         resulting_dataset = resulting_dataset.append(dataset)
+    resulting_dataset['Text'] = resulting_dataset['Text'].apply(str.strip)
     return resulting_dataset.dropna()
 
 
 def read_promise(path, resulting_dataset):
     data = arff.load(open(path, "r", encoding="cp1252"))
-    adjust_class = lambda x: "sec" if x == "SE" else "nonsec"
+    adjust_class = lambda x: SEC_LABEL if x == "SE" else NONSEC_LABEL
     data = [[row[1].strip(), adjust_class(row[2])] for row in data["data"]]
     df = pd.DataFrame(data, columns=resulting_dataset.columns)
     return resulting_dataset.append(df)
@@ -93,7 +104,7 @@ def parse_concord_xml(path, resulting_dataset, min_len):
                 ):
                     is_sec = True
                     break
-            class_ = "sec" if is_sec else "nonsec"
+            class_ = SEC_LABEL if is_sec else NONSEC_LABEL
             if len(units[start_node]) > min_len:
                 data.append([units[start_node], class_])
     df = pd.DataFrame(data, columns=resulting_dataset.columns)
@@ -109,29 +120,52 @@ def read_concord(path, resulting_dataset, min_len):
     return resulting_dataset
 
 
-def read_cchit(path, resulting_dataset): 
+def read_cchit(path, resulting_dataset):
     columns = ["Criteria #", "Criteria", "Comments"]
     cchit_data = pd.read_excel(path, header=5, usecols=columns)
-    # cchit_data = pd.read_excel(path, header=5)
-    cchit_data = cchit_data[cchit_data[columns[0]].notna()].dropna()    
-    cchit_data.to_csv("wtf.csv")
+    cchit_data = cchit_data[cchit_data[columns[0]].notna()].dropna()
 
-    prepare_label = lambda x: "sec" if "SC" in x else "nonsec"
-    prepare_text = lambda x: f"{x[0].strip()} {x[1].strip()}".replace("\n", " ") if type(x[1]) == str else x[0].strip().replace("\n", " ")
+    prepare_label = lambda criteria: SEC_LABEL if "SC" in criteria else NONSEC_LABEL
+
+    def prepare_text(texts):
+        if type(texts[1]) == str:
+            return f"{texts[0].strip()} {texts[1].strip()}".replace("\n", " ")
+        else:
+            return texts[0].strip().replace("\n", " ")
+
     labels = cchit_data[columns[0]].map(prepare_label)
     texts = cchit_data[columns[1:]].apply(prepare_text, axis=1)
 
-    data = {resulting_dataset.columns[0]: texts, resulting_dataset.columns[1]: labels} 
+    data = {resulting_dataset.columns[0]: texts,
+            resulting_dataset.columns[1]: labels}
     df = pd.DataFrame(data).dropna()
     return resulting_dataset.append(df)
+
+
+def read_owasp(path, resulting_dataset):
+    owasp_data = pd.read_csv(path, sep=",", usecols=["req_description"])
+    owasp_data = owasp_data.rename(columns={"req_description": "Text"})
+
+    def prepare_text(text):
+        verify_pattern = "^(Verify that)|^(Verify)"
+        link_pattern = "\(\[(C\d+(, )*)+].*\)$"
+        text = re.sub(f'{verify_pattern}|{link_pattern}', "", text).strip()
+        return text.title()
+
+    owasp_data["Text"] = owasp_data["Text"].apply(prepare_text)
+    owasp_data["Label"] = SEC_LABEL
+    return resulting_dataset.append(owasp_data)
+
 
 def read_datasets(args):
     columns = ["Text", "Label"]
     resulting_dataset = pd.DataFrame(columns=columns)
     resulting_dataset = read_seqreq(args.sec_req, resulting_dataset)
     resulting_dataset = read_promise(args.promise, resulting_dataset)
-    resulting_dataset = read_concord(args.concord, resulting_dataset, args.min_len)
+    resulting_dataset = read_concord(
+        args.concord, resulting_dataset, args.min_len)
     resulting_dataset = read_cchit(args.cchit, resulting_dataset)
+    resulting_dataset = read_owasp(args.owasp, resulting_dataset)
 
     resulting_dataset = resulting_dataset.drop_duplicates()
     resulting_dataset.to_csv(args.o, sep="\t", index=False)
